c379f67ef67b3a7cfae453bf324d0dbeac715705
Justin Cranford <justincranford@hotmail.com>
2025-08-04 21:04:14 -0400

    Cleaned up and added comments

--- import_encrypt_decrypt_test.go
+++ import_encrypt_decrypt_test.go
@@ -16,83 +16,82 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type tc struct {
-	key any
+type testCaseJWE struct {
+	raw any // Non-public raw key => Secret Key (AES) or Private Key (ECDH, RSA)
 	enc jwa.ContentEncryptionAlgorithm
 	alg jwa.KeyEncryptionAlgorithm
 }
 
-func Test_Import_Encrypt(t *testing.T) {
-	ecdhTC := ecdhTestCase(t, ecdh.P256(), jwa.A256GCM(), jwa.ECDH_ES_A256KW())
-	rsaTC := rsaTestCase(t, 2048, jwa.A256GCM(), jwa.RSA_OAEP_256())
-	aesTC := aesTestCase(t, 256, jwa.A256GCM(), jwa.A256KW())
+func Test_Import_Encrypt_Decrypt(t *testing.T) {
+	testCasesJWE := []testCaseJWE{
+		generateJWETestCaseECDH(t, ecdh.P256(), jwa.A256GCM(), jwa.ECDH_ES_A256KW()),
+		generateJWETestCaseRSA(t, 2048, jwa.A256GCM(), jwa.RSA_OAEP_256()),
+		generateJWETestCaseAES(t, 256, jwa.A256GCM(), jwa.A256KW()),
+	}
 
 	plaintext := []byte("Hello, World!")
-	for _, tc := range []tc{ecdhTC, rsaTC, aesTC} {
-		t.Run(tc.alg.String(), func(t *testing.T) {
-			jweJWK := importJWK(t, tc.key, tc.enc, tc.alg)
-
-			jweMessage, jweMessageBytes := encrypt(t, jweJWK, plaintext)
-			t.Logf("JWE Message:\n%s", string(jweMessageBytes))
-
-			decrypted := decrypt(t, jweJWK, jweMessage)
-
-			require.Equal(t, plaintext, decrypted, "decrypted text should match original plaintext")
-			t.Logf("Successfully encrypted and decrypted with %s", tc.alg.String())
+	for _, testCaseJWE := range testCasesJWE {
+		t.Run(testCaseJWE.alg.String(), func(t *testing.T) {
+			nonPublic := Import(t, testCaseJWE.raw, testCaseJWE.enc, testCaseJWE.alg)
+			encrypted := Encrypt(t, nonPublic, plaintext) // Pre v3.0.8 accepted private JWK for encrypt
+			decrypted := Decrypt(t, nonPublic, encrypted)
+			require.Equal(t, plaintext, decrypted, "decrypted must match original")
 		})
 	}
 }
 
-func aesTestCase(t *testing.T, keyLengthBits int, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) tc {
+func generateJWETestCaseAES(t *testing.T, keyLengthBits int, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) testCaseJWE {
 	aesSecretKey := make([]byte, keyLengthBits/8)
 	_, err := rand.Read(aesSecretKey)
-	require.NoError(t, err, "failed to generate AES secret key")
-	return tc{key: aesSecretKey, enc: enc, alg: alg}
+	require.NoError(t, err, "failed to generate raw AES secret key for JWE test case")
+	return testCaseJWE{raw: aesSecretKey, enc: enc, alg: alg}
 }
 
-func rsaTestCase(t *testing.T, keyLengthBits int, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) tc {
+func generateJWETestCaseRSA(t *testing.T, keyLengthBits int, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) testCaseJWE {
 	rsaPrivateKey, err := rsa.GenerateKey(rand.Reader, keyLengthBits)
-	require.NoError(t, err, "failed to generate RSA private key")
-	return tc{key: rsaPrivateKey, enc: enc, alg: alg}
+	require.NoError(t, err, "failed to generate raw RSA private key for JWE test case")
+	return testCaseJWE{raw: rsaPrivateKey, enc: enc, alg: alg}
 }
 
-func ecdhTestCase(t *testing.T, ecdhCurve ecdh.Curve, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) tc {
+func generateJWETestCaseECDH(t *testing.T, ecdhCurve ecdh.Curve, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) testCaseJWE {
 	ecdhPrivateKey, err := ecdhCurve.GenerateKey(rand.Reader)
-	require.NoError(t, err, "failed to generate ECDH private key")
-	return tc{key: ecdhPrivateKey, enc: enc, alg: alg}
+	require.NoError(t, err, "failed to generate raw ECDH private key for JWE test case")
+	return testCaseJWE{raw: ecdhPrivateKey, enc: enc, alg: alg}
 }
 
-func importJWK(t *testing.T, key any, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) jwk.Key {
-	recipientJWK, err := jwk.Import(key)
-	require.NoError(t, err, "failed to import key into JWK")
+func Import(t *testing.T, raw any, enc jwa.ContentEncryptionAlgorithm, alg jwa.KeyEncryptionAlgorithm) jwk.Key {
+	nonPublicJWK, err := jwk.Import(raw)
+	require.NoError(t, err, "failed to import raw key into JWK")
 
 	kid, err := uuid.NewV7()
-	require.NoError(t, err, "failed to generate UUID for JWK 'kid'")
+	require.NoError(t, err, "failed to generate UUIDv7 for recipient JWK 'kid'")
 
-	err = recipientJWK.Set(jwk.KeyIDKey, kid.String())
+	err = nonPublicJWK.Set(jwk.KeyIDKey, kid.String())
 	require.NoError(t, err, "failed to set 'kid' in recipient JWK")
-	err = recipientJWK.Set(jwk.AlgorithmKey, alg)
+	err = nonPublicJWK.Set(jwk.AlgorithmKey, alg)
 	require.NoError(t, err, "failed to set 'alg' in recipient JWK")
-	err = recipientJWK.Set("enc", enc)
+	err = nonPublicJWK.Set("enc", enc)
 	require.NoError(t, err, "failed to set 'enc' in recipient JWK")
-	err = recipientJWK.Set("iat", time.Now().UTC().Unix())
+	err = nonPublicJWK.Set("iat", time.Now().UTC().Unix())
 	require.NoError(t, err, "failed to set 'iat' in recipient JWK")
-	err = recipientJWK.Set("exp", time.Now().UTC().Unix()+(365*24*60*60)) // 365 days expiration (in seconds)
+	err = nonPublicJWK.Set("exp", time.Now().UTC().Unix()+(365*24*60*60)) // 365 days expiration (in seconds)
 	require.NoError(t, err, "failed to set 'exp' in recipient JWK")
-	err = recipientJWK.Set(jwk.KeyUsageKey, jwk.ForEncryption.String())
+	err = nonPublicJWK.Set(jwk.KeyUsageKey, jwk.ForEncryption.String())
 	require.NoError(t, err, "failed to set 'use' in recipient JWK")
-	err = recipientJWK.Set(jwk.KeyOpsKey, jwk.KeyOperationList{jwk.KeyOpEncrypt, jwk.KeyOpDecrypt})
+	err = nonPublicJWK.Set(jwk.KeyOpsKey, jwk.KeyOperationList{jwk.KeyOpEncrypt, jwk.KeyOpDecrypt})
 	require.NoError(t, err, "failed to set 'key_ops' in recipient JWK")
 
-	recipientJWKBytes, err := json.Marshal(recipientJWK)
+	nonPublicJWKBytes, err := json.Marshal(nonPublicJWK)
 	require.NoError(t, err, "failed to marshal recipient JWK")
-	t.Logf("JWE JWK:\n%s", string(recipientJWKBytes))
+	t.Logf("JWE JWK:\n%s", string(nonPublicJWKBytes))
 
-	return recipientJWK
+	return nonPublicJWK
 }
 
-func encrypt(t *testing.T, recipientJWK jwk.Key, clearBytes []byte) (*jwe.Message, []byte) {
-	require.NotEmpty(t, clearBytes, "clearBytes can't be empty")
+// recipientJWK should be a secret JWK or public JWK
+// recipientJWK could be a private JWK in v3.0.0 through v3.0.7, but v3.0.8+ rejects it
+func Encrypt(t *testing.T, recipientJWK jwk.Key, plaintext []byte) *jwe.Message {
+	require.NotEmpty(t, plaintext, "plaintext can't be empty")
 
 	jweProtectedHeaders := jwe.NewHeaders()
 	err := jweProtectedHeaders.Set("iat", time.Now().UTC().Unix())
@@ -101,7 +100,7 @@ func encrypt(t *testing.T, recipientJWK jwk.Key, clearBytes []byte) (*jwe.Messag
 	jweEncryptOptions := make([]jwe.EncryptOption, 0, 2)
 	jweEncryptOptions = append(jweEncryptOptions, jwe.WithProtectedHeaders(jweProtectedHeaders))
 
-	kid, enc, alg := extractKidEncAlg(t, recipientJWK)
+	kid, enc, alg := getKidEncAlgFromJWK(t, recipientJWK)
 
 	jweProtectedHeaders = jwe.NewHeaders()
 	jweProtectedHeaders.Set(jwk.KeyIDKey, kid)
@@ -109,25 +108,23 @@ func encrypt(t *testing.T, recipientJWK jwk.Key, clearBytes []byte) (*jwe.Messag
 	jweProtectedHeaders.Set(jwk.AlgorithmKey, alg)
 	jweEncryptOptions = append(jweEncryptOptions, jwe.WithKey(alg, recipientJWK, jwe.WithPerRecipientHeaders(jweProtectedHeaders)))
 
-	jweMessageBytes, err := jwe.Encrypt(clearBytes, jweEncryptOptions...)
-	require.NoError(t, err, fmt.Errorf("failed to encrypt clearBytes: %w", err))
+	jweMessageBytes, err := jwe.Encrypt(plaintext, jweEncryptOptions...)
+	require.NoError(t, err, fmt.Errorf("failed to encrypt plaintext: %w", err))
+	t.Logf("JWE Message:\n%s", string(jweMessageBytes))
 
 	jweMessage, err := jwe.Parse(jweMessageBytes)
 	require.NoError(t, err, fmt.Errorf("failed to parse JWE message bytes: %w", err))
 
-	return jweMessage, jweMessageBytes
+	return jweMessage
 }
 
-func decrypt(t *testing.T, recipientJWK jwk.Key, jweMessage *jwe.Message) []byte {
+func Decrypt(t *testing.T, recipientJWK jwk.Key, jweMessage *jwe.Message) []byte {
 	require.NotEmpty(t, jweMessage, "JWE message can't be empty")
-
-	var alg jwa.KeyEncryptionAlgorithm
-	err := jweMessage.ProtectedHeaders().Get(jwk.AlgorithmKey, &alg)
-	require.NoError(t, err, "failed to get algorithm from key")
 
 	jweMessageBytes, err := jweMessage.MarshalJSON()
 	require.NoError(t, err, "failed to marshal JWE message to JSON")
 
+	_, _, alg := getKidEncAlgFromJWK(t, recipientJWK)
 	jweDecryptOptions := []jwe.DecryptOption{jwe.WithKey(alg, recipientJWK)}
 
 	decryptedBytes, err := jwe.Decrypt(jweMessageBytes, jweDecryptOptions...)
@@ -136,8 +133,8 @@ func decrypt(t *testing.T, recipientJWK jwk.Key, jweMessage *jwe.Message) []byte
 	return decryptedBytes
 }
 
-// extractKidEncAlg extracts 'kid', 'enc', and 'alg' headers from recipient JWK. All 3 are assumed to be present in the JWK.
-func extractKidEncAlg(t *testing.T, recipientJWK jwk.Key) (string, jwa.ContentEncryptionAlgorithm, jwa.KeyEncryptionAlgorithm) {
+// getKidEncAlgFromJWK extracts 'kid', 'enc', and 'alg' headers from recipient JWK. All 3 are assumed to be present in the JWK.
+func getKidEncAlgFromJWK(t *testing.T, recipientJWK jwk.Key) (string, jwa.ContentEncryptionAlgorithm, jwa.KeyEncryptionAlgorithm) {
 	var kid string
 	err := recipientJWK.Get(jwk.KeyIDKey, &kid)
 	require.NoError(t, err, "failed to get 'kid' from recipient JWK")
